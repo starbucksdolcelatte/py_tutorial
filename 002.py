d0d5e643be4c39161ed38325c6c29fd7b485445f
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-02-15 10:50:48 +0000

    types

---
+++ 002.py
@@ -0,0 +1,15 @@
+#002.py
+
+a = 10 # int
+b = 10.1 # float
+c = 'hello' # str
+d = True # boolean
+e = (100, 200, 300) # tuple
+f = [100, 200, 300] # list
+g = {'one' : 1, 'two' : 2} #dictionary
+h = {1, 2, 3, 4, 5}
+
+print(c)
+print(type(c))
+print(dir(c))
+
