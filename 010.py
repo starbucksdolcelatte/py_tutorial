8ece5799dceb7901af2265c93e599d2cda756555
starbucksdolcelatte <seoyoon9535@gmail.com>
2019-02-16 02:35:52 +0000

    All about dict

---
+++ 010.py
@@ -0,0 +1,17 @@
+# dict
+d = {'one':1, 'two':2}
+#{key:value, key:value}
+
+print(d['one'])
+print(type(d))
+print(dir(d))
+print(d.values())
+print(d.keys())
+print(d.items())
+
+'''
+1. dict 안에는 다른 object 들이 들어갈 수 있다.
+2. 값(주소값)을 변화시킬 수 없습니다.
+3. 순서가 있습니다.
+참고. dict를 JSON 형식으로 쓰기도 함
+'''
